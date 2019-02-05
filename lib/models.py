98db9234ae79a7a565ed5f76b8a2f14445dd4219
andong <puppetect@gmail.com>
2019-02-05 23:06:20 +0800

    add class A2CConv1d

--- lib/models.py
+++ lib/models.py
@@ -68,3 +68,42 @@ class DQNConv1d(nn.Module):
         val = self.val(conv_out)
         adv = self.adv(conv_out)
         return val + adv - torch.mean(adv, dim=1, keepdim=True)  # (N, A)
+
+
+class A2CConv1d(nn.Module):
+    """Dueling DQN
+    :math::
+    \text{Q}(s,a) = \text{V}(s)+\text{A}(s,a) - \frac{1}{N}\textstyle\sum_{k}\text{A}(s,k)
+    """
+
+    def __init__(self, obs_shape, actions_n):
+        super().__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv1d(obs_shape[0], 128, 5),  # (N, C, L) -> (N, 128, L-4)
+            nn.ReLU(),
+            nn.Conv1d(128, 128, 5),  # (N, 128, L-4) -> (N, 128, L-8)
+            nn.ReLU()
+        )
+
+        out_size = self.get_conv_out(obs_shape)
+
+        self.policy = nn.Sequential(
+            nn.Linear(out_size, 512),  # (N, 128*(L-8)) -> (N, 512)
+            nn.ReLU(),
+            nn.Linear(512, 1)  # (N, 512) -> (N, 1)
+        )
+
+        self.value = nn.Sequential(
+            nn.Linear(out_size, 512),  # (N, 128*(L-8)) -> (N, 512)
+            nn.ReLU(),
+            nn.Linear(512, actions_n)  # (N, 512) -> (N, A)
+        )
+
+    def get_conv_out(self, shape):
+        out = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(out.shape))
+
+    def forward(self, x):
+        conv_out = self.conv(x).view(x.shape[0], -1)
+        return self.policy(conv_out), self.value(conv_out)  # (N, A)
