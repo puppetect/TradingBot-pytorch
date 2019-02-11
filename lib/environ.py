9e18ef12de47a0b9d0261d8bf90eb4858fc83603
andong <puppetect@gmail.com>
2019-02-12 00:01:25 +0800

    make the reward calculation different between train and play and return self.have_position as info

--- lib/environ.py
+++ lib/environ.py
@@ -11,11 +11,12 @@ class Actions(enum.Enum):
 
 
 class State:
-    def __init__(self, bars_count, commission, reset_on_sell, reward_on_empty):
+    def __init__(self, bars_count, commission, reset_on_sell, reward_on_empty, play):
         self.bars_count = bars_count
         self.commission = commission
         self.reset_on_sell = reset_on_sell
         self.reward_on_empty = reward_on_empty
+        self.play = play
 
     def reset(self, prices, factors, offset):
         assert offset >= self.bars_count - 1
@@ -26,12 +27,16 @@ class State:
 
     @property
     def shape(self):
-        return (58, self.bars_count)
+        return (21, self.bars_count)
 
     def encode(self):
         res = np.zeros(shape=self.shape, dtype=np.float32)
-        for idx in range(len(self.factors.columns)):
+        factors_count = len(self.factors.columns)
+        for idx in range(factors_count):
             res[idx] = self.factors.iloc[self.offset - self.bars_count:self.offset, idx]
+        if self.have_position:
+            res[factors_count] = 1.0
+            res[factors_count + 1] = (self._cur_close() - self.open_price) / self.open_price
         return res
 
     def step(self, action):
@@ -49,11 +54,15 @@ class State:
         self.offset += 1
         tmr_close = self._close()
         if self.have_position:
-            reward += 100 * (tmr_close - close) / close
+            if self.play:
+                reward += 100 * (tmr_close - close) / close
+            else:
+                reward += max(-10, min(10, (100 * (tmr_close - close) / close)**3))
         if self.reward_on_empty and not self.have_position:
             reward -= 100 * (tmr_close - close) / close
         done |= self.offset >= len(self.prices) - 1
-        return reward, done
+        info = {'have_position': int(self.have_position)}
+        return reward, done, info
 
     def _close(self):
         return self.prices.ix[self.offset, 'close']
@@ -63,10 +72,10 @@ class StockEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, prices, bars_count=100, commission=0.00025, reset_on_sell=True,
-                 random_ofs_on_reset=True, reward_on_empty=False):
+                 random_ofs_on_reset=True, reward_on_empty=False, play=False):
         self.prices = prices[0]
         self.factors = prices[1]
-        self.state = State(bars_count, commission, reset_on_sell, reward_on_empty)
+        self.state = State(bars_count, commission, reset_on_sell, reward_on_empty, play)
         self.action_space = gym.spaces.Discrete(n=len(Actions))
         self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf,
                                                 shape=self.state.shape, dtype=np.float32)
@@ -83,9 +92,8 @@ class StockEnv(gym.Env):
 
     def step(self, action_idx):
         action = Actions(action_idx)
-        reward, done = self.state.step(action)
+        reward, done, info = self.state.step(action)
         obs = self.state.encode()
-        info = {'offset': self.state.offset}
         return obs, reward, done, info
 
     def render(self, mode='human', close=False):
